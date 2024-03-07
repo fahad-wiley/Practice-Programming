9840bd2378b6e6619c2271a8861bf5434e6d3c4a
fahad-wiley <fhewad@wiley.com>
2024-03-07 15:38:19 +0000

    Completed HealthyHearts

--- ExercisesBasicProgrammingConcepts/src/HealthyHearts.java
+++ ExercisesBasicProgrammingConcepts/src/HealthyHearts.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class HealthyHearts {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte age;
+
+        while (true) {
+            System.out.print("What is your age? ");
+            age = Byte.parseByte(sc.nextLine());
+            if (age > 0) break;
+            System.out.println("Age cannot be negative!");
+        }
+
+        int maxHeartRate = 220-age;
+        System.out.println("Your maximum heart rate should be " + (maxHeartRate) + " beats per minute.");
+        System.out.println("Your target HR Zone is " + ((int) Math.ceil((0.5*maxHeartRate))) + " - " + ((int) Math.ceil(0.85*maxHeartRate)) + " beats per minute.");
 
     }
 }
